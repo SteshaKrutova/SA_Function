6946d5a51865b857bfd69a1afb3fa91900aa1c79
Krutova Stefania <mira.kru@yandex.ru>
2024-04-22 22:59:34 +0300

    added NumberEvenNumbers

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using System.Reflection.Metadata;
+
+Console.WriteLine("Hello, World!");
 // Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 void Infinity (){
@@ -12,4 +15,36 @@ void Infinity (){
         }
     }
 }
+
 Infinity();
+Console.WriteLine();
+
+// Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] CreateArrayAndWatch(int size)
+{
+    int[] array = new int[size];
+    for (int i=0;i<size;i++){
+        array[i]=new Random().Next(0,10);
+    }
+    for (int i=0;i<size;i++){
+        Console.Write(array[i]);
+    }
+    return array;
+}
+
+int NumberEvenNumbers(int[] array)
+{
+    int result =0;
+
+    for (int i=0;i<array.Length;i++){
+        if (array[i]%2==0){
+            result+=1;
+        }
+    }
+    Console.WriteLine();
+    return result;
+
+}
+
+Console.WriteLine(NumberEvenNumbers(CreateArrayAndWatch(3)));
