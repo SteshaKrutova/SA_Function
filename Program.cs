5f39bf36f59f3e4eabddc323d2dd90e95eadc96d
Krutova Stefania <mira.kru@yandex.ru>
2024-04-22 22:28:49 +0300

    give Infinity

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+void Infinity (){
+    bool bol = false;
+    while (bol==false){
+        System.Console.Write("Please write someone character: ");
+        string str = System.Console.ReadLine();
+        if (str=="q"){
+            bol=true;
+            Console.Write("It's all");
+        }
+    }
+}
+Infinity();
